4501297fc2ae0b5fe2f5c604710bd8621b57dcfd
Matheus Lopes <mlopes.matheus@gmail.com>
2023-03-23 12:37:37 -0300

    init project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.my_coffees
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:my_coffees/app/app.dart';
+import 'package:my_coffees/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
