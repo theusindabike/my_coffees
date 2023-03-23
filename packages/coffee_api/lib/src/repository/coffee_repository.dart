57c7e68624bcbc3e3620e4e2ee459db36d5d87f9
Matheus Lopes <mlopes.matheus@gmail.com>
2023-03-23 15:57:58 -0300

    feat: adding coffee repository

--- packages/coffee_api/lib/src/repository/coffee_repository.dart
+++ packages/coffee_api/lib/src/repository/coffee_repository.dart
@@ -1,1 +1,12 @@
+import 'package:coffee_api/coffee_api.dart';
 
+class CoffeeRepository {
+  CoffeeRepository({AlexflipnoteCoffeeAPI? alexflipnoteCoffeeAPI})
+      : _coffeeAPI = alexflipnoteCoffeeAPI ?? AlexflipnoteCoffeeAPI();
+
+  final AlexflipnoteCoffeeAPI _coffeeAPI;
+
+  Future<Coffee> getRandomCoffee() {
+    return _coffeeAPI.getRandomCoffee();
+  }
+}
